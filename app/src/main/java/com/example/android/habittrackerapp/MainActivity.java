b502b6c47eac24bdc49997a6a7833999e469b2d2
dell-pc <falak.p19@gmail.com>
2018-12-31 22:49:44 +0530

    Hello World!!!

---
+++ app/src/main/java/com/example/android/habittrackerapp/MainActivity.java
@@ -0,0 +1,162 @@
+package com.example.android.habittrackerapp;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private ContentResolver mContentResolver;
+    private ContentValues mContentValues;
+    private String[] projection;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContentResolver = getContentResolver();
+        mContentValues = new ContentValues();
+        projection = new String[]{
+                HabitContract.MeditationEntry._ID,
+                HabitContract.MeditationEntry.COLUMN_DID_MEDITATION,
+                HabitContract.MeditationEntry.COLUMN_DATE,
+                HabitContract.MeditationEntry.COLUMN_MEDITATION_LENGTH};
+
+//        insert("Yes", 4);
+//        update();
+//        delete();
+//        deleteDatabase();
+//        deleteAll();
+        logQueryResults(queryAll());
+//        logQueryResults(queryWhere());
+//        logQueryResults(rubricCompliantQuery());
+    }
+
+    /*
+     * Get current time in YYYYMMDDHHMMSS format
+     */
+    private String getCurrentTime() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat mdformat = new SimpleDateFormat("yyyyMMddhhmmss", Locale.US);
+        String timestamp = mdformat.format(calendar.getTime()) + "L";
+        return timestamp;
+    }
+
+    /*
+     * Returns all rows
+     * Uses content provider no need to get
+     * readable database
+     */
+    private Cursor queryAll() {
+        return mContentResolver.query(
+                HabitContract.MeditationEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                null);
+    }
+
+    /*
+     * Follows rubric
+     * returns Cursor
+     * gets a readable data storage
+     */
+    private Cursor rubricCompliantQuery() {
+        SQLiteDatabase db = new HabitDB(this).getReadableDatabase();
+        return db.query(
+                HabitContract.MeditationEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null);
+    }
+
+    /*
+     * Will return rows with particular selection arguments
+     * Make sure to append L for long in date
+     * Also change the time stamp based on data on your system
+     */
+    private Cursor queryWhere() {
+        return mContentResolver.query(
+                HabitContract.MeditationEntry.CONTENT_URI,
+                projection,
+                HabitContract.MeditationEntry.COLUMN_DATE + " = ?",
+                new String[]{"20160917080554L"},
+                null);
+    }
+
+    // Simple logger
+    private void logQueryResults(Cursor cursor) {
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                do {
+                    String did_meditation = cursor.getString(cursor.getColumnIndex(HabitContract.MeditationEntry.COLUMN_DID_MEDITATION));
+                    long date = cursor.getLong(cursor.getColumnIndex(HabitContract.MeditationEntry.COLUMN_DATE));
+                    int length = cursor.getInt(cursor.getColumnIndex(HabitContract.MeditationEntry.COLUMN_MEDITATION_LENGTH));
+                    Log.d(LOG_TAG, "Meditated " + did_meditation + " on " + date + " for " + length + " mins");
+                } while (cursor.moveToNext());
+                cursor.close();
+            }
+        }
+    }
+
+    /*
+     * update the uri based on data on your system
+     */
+    private void update() {
+        mContentValues.clear();
+        mContentValues.put(HabitContract.MeditationEntry.COLUMN_DID_MEDITATION, "Yes");
+        mContentValues.put(HabitContract.MeditationEntry.COLUMN_MEDITATION_LENGTH, 8);
+        Uri uri = ContentUris.withAppendedId(HabitContract.MeditationEntry.CONTENT_URI, 20160917104605L);
+        int rowsUpdated = mContentResolver.update(uri, mContentValues, null, null);
+        Log.d(LOG_TAG, "Rows updated: " + rowsUpdated);
+    }
+
+    private void insert(String yesOrNo, int time) {
+        mContentValues.clear();
+        mContentValues.put(HabitContract.MeditationEntry.COLUMN_DID_MEDITATION, yesOrNo);
+        mContentValues.put(HabitContract.MeditationEntry.COLUMN_MEDITATION_LENGTH, time);
+        mContentValues.put(HabitContract.MeditationEntry.COLUMN_DATE, getCurrentTime());
+        mContentResolver.insert(HabitContract.MeditationEntry.CONTENT_URI, mContentValues);
+    }
+
+    // Make sure to append L for long in date
+    // update time stamp as per your system
+    private void delete() {
+        int rowsDeleted;
+        rowsDeleted = mContentResolver.delete(
+                HabitContract.MeditationEntry.CONTENT_URI,
+                HabitContract.MeditationEntry.COLUMN_DATE + " = ?",
+                new String[]{"20160917104325L"});
+        Log.d(LOG_TAG, "# of Rows Deleted: " + rowsDeleted);
+    }
+
+    private void deleteAll() {
+        int rowsDeleted;
+        rowsDeleted = mContentResolver.delete(
+                HabitContract.MeditationEntry.CONTENT_URI,
+                null,
+                null);
+        Log.d(LOG_TAG, "# of Rows Deleted: " + rowsDeleted);
+    }
+
+    // Delete the whole database
+    private void deleteDatabase() {
+        new HabitDB(this).deleteDatabase(this);
+    }
+}
---
+++ app/src/main/java/com/example/android/habittrackerapp/Provider.java
@@ -0,0 +1,168 @@
+package com.example.android.habittrackerapp;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+public class Provider extends ContentProvider {
+    private static final String LOG_TAG = Provider.class.getSimpleName();
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private HabitDB mHabitDB;
+    private static final int MEDITATION = 100;
+    private static final int MEDITATION_ID = 101;
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = HabitContract.CONTENT_AUTHORITY;
+        matcher.addURI(authority, HabitContract.MeditationEntry.TABLE_NAME, MEDITATION);
+        matcher.addURI(authority, HabitContract.MeditationEntry.TABLE_NAME + "/#", MEDITATION_ID);
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mHabitDB = new HabitDB(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri,
+                        String[] projection,
+                        String selection,
+                        String[] selectionArgs,
+                        String sortOrder) {
+        Cursor retCursor;
+        //TODO: REMOVE THIS
+//        Log.d(LOG_TAG, "query(): where " + selection + " is " + selectionArgs[0]);
+        switch (sUriMatcher.match(uri)) {
+            case MEDITATION:
+                retCursor = mHabitDB.getReadableDatabase().query(
+                        HabitContract.MeditationEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                return retCursor;
+            case MEDITATION_ID:
+                String id = String.valueOf(ContentUris.parseId(uri)) + "L";
+                retCursor = mHabitDB.getReadableDatabase().query(
+                        HabitContract.MeditationEntry.TABLE_NAME,
+                        projection,
+                        HabitContract.MeditationEntry.COLUMN_DATE + " = ?",
+                        new String[]{id},
+                        null,
+                        null,
+                        sortOrder);
+                return retCursor;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case MEDITATION:
+                return HabitContract.MeditationEntry.CONTENT_DIR_TYPE;
+            case MEDITATION_ID:
+                return HabitContract.MeditationEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        final SQLiteDatabase db = mHabitDB.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        Uri returnUri;
+        switch (match) {
+            case MEDITATION:
+                long _id = db.insert(HabitContract.MeditationEntry.TABLE_NAME, null, contentValues);
+                if (_id > 0) {
+                    returnUri = HabitContract.MeditationEntry.buildProductUri(_id);
+                } else {
+                    throw new android.database.SQLException("Failed to insert row into: " + uri);
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mHabitDB.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int numDeleted;
+        //TODO REMOVE THIS
+//        Log.d(LOG_TAG, "delete(): where " + selection + " is " + selectionArgs[0]);
+        switch (match) {
+            case MEDITATION:
+                Log.d(LOG_TAG, "delete() dir");
+                numDeleted = db.delete(
+                        HabitContract.MeditationEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            case MEDITATION_ID:
+                Log.d(LOG_TAG, "delete() type");
+                String id = String.valueOf(ContentUris.parseId(uri)) + "L";
+                numDeleted = db.delete(
+                        HabitContract.MeditationEntry.TABLE_NAME,
+                        HabitContract.MeditationEntry.COLUMN_DATE + " = ?",
+                        new String[]{id});
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        if (numDeleted > 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return numDeleted;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mHabitDB.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int numUpdated;
+        if (contentValues == null) {
+            throw new IllegalArgumentException("Cannot have null content values");
+        }
+        switch (match) {
+            case MEDITATION:
+                Log.d(LOG_TAG, "update() Dir");
+                numUpdated = db.update(HabitContract.MeditationEntry.TABLE_NAME,
+                        contentValues,
+                        selection,
+                        selectionArgs);
+                break;
+            case MEDITATION_ID:
+                String id = String.valueOf(ContentUris.parseId(uri)) + "L";
+                numUpdated = db.update(HabitContract.MeditationEntry.TABLE_NAME,
+                        contentValues,
+                        HabitContract.MeditationEntry.COLUMN_DATE + " = ?",
+                        new String[]{id});
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        if (numUpdated > 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return numUpdated;
+    }
+}
